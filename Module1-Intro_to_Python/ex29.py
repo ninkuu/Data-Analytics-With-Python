eabdbb426c75893e5ce4e96125ab261c9916298f
Ninh Khuu <ninh.khuu@wsu.edu>
2023-02-09 01:34:33 -0800

    more learning

---
+++ Module1-Intro_to_Python/ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("there are more cats than people!")
+    
+if people > cats:
+    print("there are more people than cats")
+    
+if people < dogs:
+    print("There are more dogs than people")
+    
+if people > dogs:
+    print("there are more people than dogs")
+    
+dogs += 5
+
+if people >= dogs:
+    print("There are greater than or equal number of people to dogs")
+    
+if people <= dogs:
+    print("There are lesser than or equal number of people to dogs")
+    
+if people == dogs:
+    print("there are an equal number of dogs to people")

6809cb931d735d2cd7721fa1058da34e3394cedc
Ninh Khuu <ninh.khuu@wsu.edu>
2023-02-13 22:15:29 -0800

    project

--- Module1-Intro_to_Python/project/Project1.py
+++ Module1-Intro_to_Python/project/Project1.py
@@ -41,8 +41,6 @@ COLUMNS = {
     9: 'Horizontal_Distance_To_Fire_Points'
 }
 '''
-from sys import argv
-
 #Global variables that are permitted and associated with the covtype.data file
 
 AREAS = {
@@ -67,41 +65,105 @@ COLUMNS = {
 }
 
 file = open('covtype.data')
-
-for area in AREAS.keys():
-    if area != 0:
-        print(
-
-
-def pull_data(area_dictionary, line_cols):
-    for column_number in area_dictionary.keys():
-        area_dictionary[column_number].append(float(line_cols[column_number]))
         
-def call_report(area_dictionary):
-    for column_number in area_dictionary.keys():
-        print("Minimum {}: {}".format(COLUMNS[column_number], min (area_dictioanry[column_number])))
-        print("Maximum {}: {}".format(COLUMNS[column_number], max (area_dictioanry[column_number])))
-        print("Mean {}: {}".format(COLUMNS[column_number], round((sum(area_dictioanry[column_number])/len(area_dictioanry[column_number])))))
-        print("\n")
-        
-def main()        
+# def get_statistics(area_dictionary):
+#     for column_number in area_dictionary.keys():
+#         print("Minimum {}: {}".format(COLUMNS[column_number], min (area_dictionary[column_number])))
+#         print("Maximum {}: {}".format(COLUMNS[column_number], max (area_dictionary[column_number])))
+#         print("Mean {}: {}".format(COLUMNS[column_number], round((sum(area_dictionary[column_number])/len(area_dictionary[column_number])))))
+#         print("\n")    
+def get_statistics(raw_data):
+    col1 = extract_column(raw_data, 0)
+    min_stats = min(col1)
+    max_stats = max(col1)
+    avg_stats = sum(col1)/len(col1)
+    return [min_stats,max_stats,avg_stats]
+
+def extract_column(raw_data,column_number):
+    return [item[column_number] for item in raw_data]
+
+def main():
+    all_array = []
+    rawah_array = []
+    neota_array = []
+    comanche_peak_array = []
+    cache_array = []
+    aggregate_array = []
     for row in file:
-        cols = row.split(".")
-        rawah, neota, comanche, cache = int(cols[11]), int(cols[12]), int(cols[13]), int(cols[14])
-        if rawah == 1:
-            pull_data(cols)
-        elif neota == 1:
-            pull_data(cols)
-        elif comanche == 1:
-            pull_data(cols)
-        elif cache == 1:
-            pull_data(cols)
-    call_report(rawah)
-    call_report(neota)
-    call_report(comanche)
-    call_report(cache)
+        cols = row.split(",")
+        if cols[10] == "0":
+            all_array.append(list(map(int,cols[0:9])))
+        elif cols[10] == "1":
+            rawah_array.append(list(map(int,cols[0:9])))
+        elif cols[10] == "2":
+            neota_array.append(list(map(int,cols[0:9])))
+        elif cols[10] == "3":
+            comanche_peak_array.append(list(map(int,cols[0:9])))
+        elif cols[10] == "4":
+            cache_array.append(list(map(int,cols[0:9])))
+        aggregate_array.append(list(map(int,cols[0:9])))
     
-    file.close()
+    print("Rawah min, max, and average:",get_statistics(rawah_array))
+    print("Neota min, max, and average:",get_statistics(rawah_array))
+    print("Comanche peak min, max, and average:",get_statistics(rawah_array))
+    print("Cache la poudre min, max, and average:",get_statistics(rawah_array))
+    print("Min, max, and average statistics for Elevation:",get_statistics(aggregate_array))
     
+                               
 if __name__ == "__main__":
     main()
+
+file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for area in AREAS.keys():
+#     if area != 0:
+#         print(area)
+
+# def pull_data(area_dictionary, line_cols):
+#     for column_number in area_dictionary.keys():
+#         area_dictionary[column_number].append(float(line_cols[column_number]))
+        
+# def call_report(area_dictionary):
+#     for column_number in area_dictionary.keys():
+#         print("Minimum {}: {}".format(COLUMNS[column_number], min (area_dictioanry[column_number])))
+#         print("Maximum {}: {}".format(COLUMNS[column_number], max (area_dictioanry[column_number])))
+#         print("Mean {}: {}".format(COLUMNS[column_number], round((sum(area_dictioanry[column_number])/len(area_dictionary[column_number])))))
+#         print("\n")
+        
+# def main()        
+#     for row in file:
+#         cols = row.split(".")
+#         rawah, neota, comanche, cache = int(cols[11]), int(cols[12]), int(cols[13]), int(cols[14])
+#         if rawah == 1:
+#             pull_data(cols)
+#         elif neota == 1:
+#             pull_data(cols)
+#         elif comanche == 1:
+#             pull_data(cols)
+#         elif cache == 1:
+#             pull_data(cols)
+#     call_report(rawah)
+#     call_report(neota)
+#     call_report(comanche)
+#     call_report(cache)
+    
+# file.close()
+    
+# if __name__ == "__main__":
+#     main()

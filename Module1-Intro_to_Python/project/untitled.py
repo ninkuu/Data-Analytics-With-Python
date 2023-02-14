26ec35d85d6fd5a70c83a92a8b454243fb113d15
Ninh Khuu <ninh.khuu@wsu.edu>
2023-02-13 22:17:03 -0800

    project

---
+++ Module1-Intro_to_Python/project/untitled.py
@@ -0,0 +1,23 @@
+'''python
+Project for Module 1
+
+Goals:
+A) Calculate the following statistics for the first 10 columns of the data file:
+    Maximum value
+    Minimum value
+    Average (Mean)
+B) Calculate the same set of statistics but for each of the seven wilderness areas. The wilderness areas are identified in columns 11-14 of the data file.
+C) Write a new file that contains a report of the statistics calculated.
+
+Rules:
+Your program must make use of the following:
+
+1) arguments passed via the command-line
+2) use of lists or dictionaries
+3) use of functions
+4) use of loops
+5) proper commenting
+6) Your program must not Use global variables (except the exceptions listed below)
+
+
+'''
